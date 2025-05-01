3f60ca321f0847b2a7ea3f0f634d5c13a1a998ad
KaushalC0des <kaushalgadekar4@gmail.com>
2025-05-01 18:22:35 +0530

    initial commit

---
+++ Main.java
@@ -0,0 +1,38 @@
+
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int[] nums = {1, 2, 3, 4};
+        int[] result = sol.productExceptSelf(nums);
+
+        System.out.println("Output: " + Arrays.toString(result));
+    }
+}
+
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int[] left = new int[nums.length];
+        int[] right = new int[nums.length];
+
+        left[0] = 1;
+        for (int i = 1; i < nums.length; i++) {
+            left[i] = left[i - 1] * nums[i - 1];
+        }
+
+        right[nums.length - 1] = 1;
+        for (int i = nums.length - 2; i >= 0; i--) {
+            right[i] = right[i + 1] * nums[i + 1];
+        }
+
+        int[] ans = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            ans[i] = left[i] * right[i];
+        }
+
+        return ans;
+    }
+}
--- mergeKSortedList.java
+++ mergeKSortedList.java
@@ -1,69 +1,12 @@
-class ListNode {
-    int val;
-    ListNode next;
-    ListNode(int x) {
-        val = x;
-    }
-}
-
-class Solution {
-    public ListNode mergeTwoList(ListNode l1, ListNode l2){
-        if (l1 == null) return l2;
-        if (l2 == null) return l1;
-
-        if(l1.val <= l2.val){
-            l1.next = mergeTwoList(l1.next, l2);
-            return l1;
-        } else {
-            l2.next = mergeTwoList(l1, l2.next);
-            return l2;
-        }
-    }
-
-    public ListNode mergeKLists(ListNode[] lists) {
-        if(lists.length == 0) return null;
-        return divideAndConquer(lists, 0, lists.length - 1);
-    }
-
-    private ListNode divideAndConquer(ListNode[] lists, int left, int right){
-        if (left == right) return lists[left];
-
-        int mid = left + (right - left) / 2;
-        ListNode l1 = divideAndConquer(lists, left, mid);
-        ListNode l2 = divideAndConquer(lists, mid + 1, right);
-        return mergeTwoList(l1, l2);
-    }
-}
-
 public class mergeKSortedList {
     public static void main(String[] args) {
-        // Creating test data
-        ListNode l1 = new ListNode(1);
-        l1.next = new ListNode(4);
-        l1.next.next = new ListNode(5);
+        int[] arr = {1,2,3,4};
 
-        ListNode l2 = new ListNode(1);
-        l2.next = new ListNode(3);
-        l2.next.next = new ListNode(4);
+        int k = arr.length;
+        System.out.println(k);
 
-        ListNode l3 = new ListNode(2);
-        l3.next = new ListNode(6);
-
-        ListNode[] lists = new ListNode[]{l1, l2, l3};
-
-        Solution sol = new Solution();
-        ListNode result = sol.mergeKLists(lists);
-
-        // Print the merged list
-        printList(result);
+        
     }
 
-    public static void printList(ListNode node) {
-        while (node != null) {
-            System.out.print(node.val + " ");
-            node = node.next;
-        }
-        System.out.println();
-    }
+    
 }
-
