df7d468e52eb4ec845b1bfff907ca0a2c1700970
KaushalC0des <kaushalgadekar4@gmail.com>
2025-05-09 19:15:29 +0530

    initial commit

---
+++ MaxArea.java
@@ -0,0 +1,30 @@
+public class MaxArea {
+
+    public static int maxArea(int[] height) {
+        int left = 0;
+        int right = height.length - 1;
+        int maxArea = 0;
+
+        while (left < right) {
+            int currentHeight = Math.min(height[left], height[right]);
+            int width = right - left;
+            int area = currentHeight * width;
+
+            maxArea = Math.max(maxArea, area);
+
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return maxArea;
+    }
+
+    public static void main(String[] args) {
+        int[] height = {1, 8, 6, 2, 5, 4, 8, 3, 7};
+        int result = maxArea(height);
+        System.out.println("Maximum area: " + result);
+    }
+}
